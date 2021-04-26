1cf19e13f65190f15dd482bd1dbb10295e40184a
DuyLuu <bdl.uit@gmail.com>
2021-04-26 23:14:00 +0700

    Connect to MongoBD

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+const mongoUri = 'mongodb+srv://admin:duyluu@cluster0.b4zbi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongoose instance')
+})
+mongoose.connection.on('error', (err) => {
+    console.log('Error connecting to mongoose', err)
+})
 
 app.get('/', (req, res) => {
     res.send('Hello world')
